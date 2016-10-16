8ae38ab46b3112f05162a56565c04e44431b40c2
Melvin Carvalho <melvincarvalho@gmail.com>
2016-10-16 15:39:16 +0200

    Add CLI arguments closes #7

--- bin/groupuris.js
+++ bin/groupuris.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
 var groupuris = require('../')
+var program = require('commander')
+
+program
+  .parse(process.argv)
 
 var agents = ['https://melvincarvalho.com/#me', 'http://csarven.ca/#i']
+console.log(program.args)
+if (program.args.length !== 0) {
+  agents = program.args
+}
 
 var turtle = groupuris.agentsToGroup(agents)
 var hash = groupuris.toSha256Base64url(turtle)
