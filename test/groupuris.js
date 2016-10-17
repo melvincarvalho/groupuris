0b532492898070bf69e882007da09b178091eb74
Melvin Carvalho <melvincarvalho@gmail.com>
2016-10-17 18:07:50 +0200

    Change FOAF -> VCARD closes #11

--- test/groupuris.js
+++ test/groupuris.js
@@ -11,9 +11,9 @@ describe('Groupuris Functions', function () {
 
     var agents = ['https://melvincarvalho.com/#me', 'http://csarven.ca/#i']
 
-    var group = '_: <https://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Group> .\n\
-_: <http://xmlns.com/foaf/0.1/member> <http://csarven.ca/#i> .\n\
-_: <http://xmlns.com/foaf/0.1/member> <https://melvincarvalho.com/#me> .\n'
+    var group = '_: <https://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/vcard/ns#Group> .\n\
+_: <http://www.w3.org/2006/vcard/ns#hasMember> <http://csarven.ca/#i> .\n\
+_: <http://www.w3.org/2006/vcard/ns#hasMember> <https://melvincarvalho.com/#me> .\n'
 
     it('test agentsToGroup([\'https://melvincarvalho.com/#me\', \'http://csarven.ca/#i\']) = \'' + group + '\'', function () {
       expect((groupuris.agentsToGroup(agents))).to.equal(group)
@@ -25,11 +25,11 @@ _: <http://xmlns.com/foaf/0.1/member> <https://melvincarvalho.com/#me> .\n'
       expect((groupuris.toSha256Base64url)).to.be.a('function')
     })
 
-    var group = '_: <https://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Group> .\n\
-_: <http://xmlns.com/foaf/0.1/member> <http://csarven.ca/#i> .\n\
-_: <http://xmlns.com/foaf/0.1/member> <https://melvincarvalho.com/#me> .\n'
+    var group = '_: <https://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/vcard/ns#Group> .\n\
+_: <http://www.w3.org/2006/vcard/ns#hasMember> <http://csarven.ca/#i> .\n\
+_: <http://www.w3.org/2006/vcard/ns#hasMember> <https://melvincarvalho.com/#me> .\n'
 
-    var base64url = 'GE7vGwCUU_3mX3PMy5cefAAZ4_zuBpFq-mHyF1CZ7nM'
+    var base64url = 'AO8kEF3c6teJ0n8fJ20_JZEosg4klzFF2vQOf-pTzkg'
 
     it('test agentsToGroup([\'' + group + '\']) = \'' + base64url + '\'', function () {
       expect((groupuris.toSha256Base64url(group))).to.equal(base64url)
